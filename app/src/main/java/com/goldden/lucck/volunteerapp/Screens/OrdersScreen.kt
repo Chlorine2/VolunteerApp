02546d5d0c8c18774f17abe1fd175ad3ec3d084c
Chroline21 <nazstikler@gmail.com>
2023-11-11 14:08:39 +0200

    ColorTheme

--- app/src/main/java/com/goldden/lucck/volunteerapp/Screens/OrdersScreen.kt
+++ app/src/main/java/com/goldden/lucck/volunteerapp/Screens/OrdersScreen.kt
@@ -1,10 +1,16 @@
 package com.goldden.lucck.volunteerapp.Screens
 
+import android.content.res.Resources.Theme
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.goldden.lucck.volunteerapp.BottomNavigationBar
 
 
 @Composable
 fun OrderScreen(){
-    Text(text = "Lox")
+    Text(text = "Lox", color = MaterialTheme.colorScheme.primary)
+
 }
--- app/src/main/java/com/goldden/lucck/volunteerapp/ui/theme/Color.kt
+++ app/src/main/java/com/goldden/lucck/volunteerapp/ui/theme/Color.kt
@@ -2,9 +2,11 @@ package com.goldden.lucck.volunteerapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val main = Color(0xFFBEF2FF     )
+val second = Color(0xFF4F7097)
+val background = Color(0xFF93A7D1)
+val highlight = Color(0xFF1BF5AF)
+
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
